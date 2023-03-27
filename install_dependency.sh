20b1773886187cfbfa061719ff547471c8d203be
Donghyun Lim <donghyun@nalbi.ai>
2023-03-28 07:04:53 +0900

    feat: diff-so-fancy

--- install_dependency.sh
+++ install_dependency.sh
@@ -3,6 +3,8 @@
 set -x # show commands
 set -e # terminate on error
 
+mkdir ~/install/
+
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -11,8 +13,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # cdhist
-git clone http://github.com/bulletmark/cdhist
-pip3 install -U cdhist/.
+git clone http://github.com/bulletmark/cdhist ~/install/cdhist
+pip3 install -U ~/install/cdhist/.
 
 # SpaceVim
 curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim
@@ -20,4 +22,28 @@ curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim
 # Vim dependency
 pip install pynvim
 pip install python-language-server
+
+# diff-so-fancy
+git clone https://github.com/so-fancy/diff-so-fancy ~/install/diff-so-fancy
+sudo ln -snf ~/install/diff-so-fancy/diff-so-fancy /usr/local/bin/diff-so-fancy
+
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
+
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.func       "146 bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+
 
