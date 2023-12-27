412819b29fb262935a1202e3b82489fa76ccbf46
Donghyun Lim <donghyunismyname@gmail.com>
2023-12-27 19:37:27 +0900

    feat: lazyvim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("n", "<leader>q", ":q<cr>")
+vim.keymap.set("n", "<leader>w", ":w<cr>")
