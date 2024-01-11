8646eae2be7cfc6c03bac4eb0ddcaba92f78c357
Donghyun Lim <donghyunismyname@gmail.com>
2024-01-11 14:20:49 +0900

    minor: emacs init

--- emacs/init.el
+++ emacs/init.el
@@ -132,7 +132,8 @@ term-mode))
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-collection evil general zenburn-theme use-package timu-spacegrey-theme timu-macos-theme doom-modeline command-log-mode)))
+   (quote
+    (evil-collection evil general zenburn-theme use-package timu-spacegrey-theme timu-macos-theme doom-modeline command-log-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
