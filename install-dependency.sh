e223ed8c14a7da3c93365b2339aa18ef4b17f41b
Donghyun Lim <donghyunismyname@gmail.com>
2024-07-19 14:22:42 +0900

    Update install-dependency.sh

--- install-dependency.sh
+++ install-dependency.sh
@@ -5,20 +5,21 @@ set -x # show commands
 
 mkdir -p ~/install/
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/install/.fzf
+~/install/.fzf/install
 
 # cdhist
- git clone --branch 3.6 http://github.com/bulletmark/cdhist ~/install/cdhist
-pip3 install -U ~/install/cdhist/.
+pip3 install cdhist
+# git clone --branch 3.6 http://github.com/bulletmark/cdhist ~/install/cdhist
+# pip3 install -U ~/install/cdhist/.
 
 # diff-so-fancy
 git clone https://github.com/so-fancy/diff-so-fancy ~/install/diff-so-fancy
 sudo ln -snf ~/install/diff-so-fancy/diff-so-fancy /usr/local/bin/diff-so-fancy
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Doom Emacs
 # git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
