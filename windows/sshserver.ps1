6664f67e255457fa933a7af2a75edc7bf5225b4a
Donghyun Lim <donghyunismyname@gmail.com>
2024-12-11 11:58:31 +0900

    Update sshserver.ps1

--- windows/sshserver.ps1
+++ windows/sshserver.ps1
@@ -1,4 +1,5 @@
 wsl --exec dbus-launch true sudo /etc/init.d/ssh start
 $wsl_ip = (wsl hostname -I).trim()
 Write-Host "WSL Machine IP: ""$wsl_ip"""
-netsh interface portproxy add v4tov4 listenport=22 connectport=2222 connectaddress=$wsl_ip
+netsh interface portproxy delete v4tov4 listenport=2222
+netsh interface portproxy add v4tov4 listenport=2222 connectport=22 connectaddress=$wsl_ip
