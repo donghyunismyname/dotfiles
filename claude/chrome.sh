f69f525befecbaddf1cdf814c4f144aca367eb2d
Donghyun Lim <donghyunismyname@gmail.com>
2026-01-20 04:34:30 +0900

    claude/chrome.sh

---
+++ claude/chrome.sh
@@ -0,0 +1,7 @@
+chrome \
+  --remote-debugging-port=9222 \
+  --user-data-dir=~/workspace/playwright \
+  --disable-backgrounding-occluded-windows \
+  --disable-background-timer-throttling \
+  --disable-renderer-backgrounding
+
