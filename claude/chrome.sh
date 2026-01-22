07360b97875180d72ab399c4aced2153fdc6f542
Donghyun Lim <donghyunismyname@gmail.com>
2026-01-22 14:39:18 +0900

    fix: support macOS in chrome.sh

--- claude/chrome.sh
+++ claude/chrome.sh
@@ -1,6 +1,14 @@
-google-chrome \
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  CHROME="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+else
+  CHROME="google-chrome"
+fi
+
+"$CHROME" \
   --remote-debugging-port=9222 \
-  --user-data-dir=$HOME/workspace/playwright \
+  --user-data-dir="$HOME/workspace/playwright" \
   --disable-backgrounding-occluded-windows \
   --disable-background-timer-throttling \
   --disable-renderer-backgrounding
